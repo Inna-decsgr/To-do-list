51142adc75b202b4df4ba03eecdb733d62c3b0ed
Inna-decsgr <anjgkwl27@gmail.com>
2023-11-12 23:21:21 +0900

    🚧 Create DarkModeContext and modify Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 import Header from './components/Header';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 const filters = ['all', 'active', 'completed']
 export default function App() {
   const [filter, setFilter] = useState(filters[0])
   return (
-    <>
+    <DarkModeProvider>
       <Header filters={filters} filter={filter} onFilterChange={filter => setFilter(filter)}/>
       <TodoList filter={filter}/>
-    </>
+    </DarkModeProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import styles from './Header.module.css'
+import { useDarkMode } from '../context/DarkModeContext';
+import { BiSolidMoon } from "react-icons/bi";
+import { BsFillEmojiSunglassesFill } from "react-icons/bs";
 
 export default function Header({filters, filter, onFilterChange}) {
+  const {darkMode, toggleDarkMode} = useDarkMode();
   return (
     <header className={styles.header}>
+      <button className={styles.toggle} onClick={toggleDarkMode}>
+        {!darkMode && <BiSolidMoon />}
+        {darkMode && <BsFillEmojiSunglassesFill />}
+      </button>
       <ul className={styles.filters}>
         {
           filters.map((value, index) => 
