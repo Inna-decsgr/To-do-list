80f6481ebb63271c3e3a3feb523fc0564fe58f49
Inna-decsgr <anjgkwl27@gmail.com>
2023-11-12 20:10:40 +0900

    🐛 Fix a bug on input and label

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BsTrashFill } from 'react-icons/bs';
+import { AiFillEdit } from 'react-icons/ai';
 
 export default function Todo({todo, onUpdate, onDelete}) {
   const {id, text, status} = todo;
@@ -10,13 +11,16 @@ export default function Todo({todo, onUpdate, onDelete}) {
     })
   }
   const handleDelete = () => onDelete(todo);
+  
   return (
-    <li key={id}>
+    <li>
       <input 
+        id={id}
         type="checkbox" 
         checked = {status === 'completed'}
         onChange={handleChange}
-      />{text}
+      />
+      <label htmlFor={id}>{text}</label>
       <button onClick={handleDelete}><BsTrashFill /></button>
     </li>
   );
